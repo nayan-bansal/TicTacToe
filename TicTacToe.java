ba07e4f9e2afec6b949c60744d4b72083d7ba4a9
= <=>
2020-10-03 13:09:16 +0530

    Added: The Board Array of size 10

---
+++ TicTacToe.java
@@ -0,0 +1,21 @@
+package Game;
+
+public class TicTacToe {
+
+	//Create a Board of Size 10 and allocate empty space for each block
+			char[] game = new char[10];
+	
+			
+	public void createBoard() {
+		game  = null;
+	}
+			
+	public static void main(String[] args) {
+		
+	//Welcome to the Tic Tac Toe Game
+		System.out.println("Welcome to the Tic Tac Toe Game");
+		
+		
+	}
+	
+}
