cdc028282227382e4d832d3433a381b8ccce18ea
= <=>
2020-10-03 13:37:34 +0530

    Added: Choice to select Letter

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,13 +1,28 @@
-package Game;
+package com.tictactoegame;
+import java.util.*;
 
 public class TicTacToe {
 
 	//Create a Board of Size 10 and allocate empty space for each block
-			char[] game = new char[10];
 	
-			
+	//Initialize Board for game		
 	public void createBoard() {
+		char[] game = new char[10];
 		game  = null;
+	}
+	
+	//Choose X or O for user
+	public void method() {
+		Scanner scan = new Scanner(System.in);
+		char choice_user = scan.next().charAt(0);
+		char choice_computer = '0';
+		
+		if(choice_user == 'X')
+			choice_computer = 'O';
+		
+		else
+			choice_computer = 'X';	
+		
 	}
 			
 	public static void main(String[] args) {
@@ -15,7 +30,11 @@ public class TicTacToe {
 	//Welcome to the Tic Tac Toe Game
 		System.out.println("Welcome to the Tic Tac Toe Game");
 		
+		TicTacToe game = new TicTacToe();
+		game.createBoard();
 		
+		System.out.println("Enter the Input: X or O");
+		game.method();
 	}
 	
 }
